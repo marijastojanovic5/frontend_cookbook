67cbdbe22cc5c84d8675028497cc65e470ce352d
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-06-09 11:07:19 -0400

    three tests passing

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -1,26 +1,22 @@
-import  reducer from "../redux/reducer"
-describe ('reducer',()=>{     
-    it ( 'handles @@INIT',()=>{
-        let action = {type: "@@INIT"}
-        expect(reducer(undefined,action )).toEqual({
-         "ingredients":  [],
-         "newIngredient":  [],
-         "recipes":  [],
-         "redirect": false,
-         "searchText": "",
-         "user": null,
-          })
-        })
-    
-    
-    // it ('handles FETCHED_INGREDIENTS', ()=>{
-    //         let mockState= {"ingredients": []}
-    //         let mockAction ={type: "FETCHED_INGREDIENTS", payload: { amount: 1,name:"Garlic", unit: "clove"}}
-    //         let result =(reducer(mockState,mockAction))
-    //       expect(result["ingredients"]).toBe( result.mockAction.payload )
-
-
-    //     })
-    })
-    
-        
+import reducer from '../redux/reducer'
+describe("reducer", () => {
+  it('handles @@INIT', () => {
+    let action = {
+      type: "@@INIT"
+    }
+    expect(reducer(undefined, action)).toEqual({ 'ingredients': [], 'newIngredient': [], 'recipes': [],
+    'redirect': false, 'user': null, 'searchText': "" })
+  })
+  it ('handles ADD_NEW_RECIPE', () => {
+    let mockState = { recipes: [] }
+    let mockAction = { type: 'ADD_NEW_RECIPE', payload: { id: 1, title: "Fried Chicken", cook_time: 30, instructions: "Bread chicken and fry in oil", picture: null, gluten_free: false, vegetarian: false, vegan: false, dairy_free: false }}
+    let result = (reducer(mockState, mockAction))
+    expect(result.recipes.length).toBe(1)
+  })
+  it( 'handles FETCHED_USER', () => {
+    let mockState = { user: {} }
+    let mockAction = { type: 'FETCHED_USER', payload: { id: 1, first_name: "Dean", last_name: 'Hildebrand', username: "dean88", bio: null, picture: null, password_digest: "h3ll0" }}
+    let result = (reducer(mockState, mockAction))
+    expect(result.user).toBe(mockAction.payload)
+  })
+})

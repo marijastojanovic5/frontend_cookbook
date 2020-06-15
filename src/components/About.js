3dbb159163dd5e91a532a8e4ed478585180e5933
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-06-15 16:51:03 -0400

    links for authors added

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,11 @@ const About = props => {
             <div className="about-created">
                 <footer>
                 <h6>Created by:</h6>
-                <p>Dean Hildebrand</p>
-                <p>Marija Stojanovic</p>
+                <p>Dean Hildebrand <a href="https://www.linkedin.com/in/hildebranddean/"><i className="fab fa-linkedin"></i></a></p>
+                {/* <a href="https://github.com/spike0161"><i className="fas fa fa-github"></i></a> */}
+                <p>Marija Stojanovic <a href="https://www.linkedin.com/in/marijastojanovic1987/"><i className="fab fa-linkedin"></i></a></p>
+                 {/* <a href="https://github.com/marijastojanovic5"><i className="fas fa fa-github" style={{font: 24}}></i></a>  */}
+
                 <p>&#169; 2020</p>
                 </footer>
 
